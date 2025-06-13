6c90f9f3d2b95cef5acb45ba671af07ab62c6d3b
teamon9161 <teamon9161@163.com>
2025-06-13 14:18:18 +0800

    [feat] impl world gotoTime

--- src/backtest.zig
+++ src/backtest.zig
@@ -18,10 +18,19 @@ const Local = struct {
             .asset = asset,
         });
     }
+
+    pub fn process_events(self: *Local, events: ?[]const Event) !void {
+        _ = .{ self, events };
+        return;
+    }
 };
 
 const Exchange = struct {
     w: *World,
+    pub fn process_events(self: *Exchange, events: ?[]const Event) !void {
+        _ = .{ self, events };
+        return;
+    }
 };
 
 pub const Options = struct {
@@ -35,23 +44,51 @@ pub const World = struct {
     assets: []const Asset,
     ex_ep: EventPool,
     local_ep: EventPool,
-    local: Local,
-    exchange: Exchange,
+    _local: ?Local = null,
+    _exchange: ?Exchange = null,
 
     pub fn init(opt: Options) World {
         const ex_ep = EventPool.init(opt.allocator);
         const local_ep = EventPool.init(opt.allocator);
-        var self = World{
+        return World{
             .time = opt.time,
             .assets = opt.assets,
             .ex_ep = ex_ep,
             .local_ep = local_ep,
-            .local = undefined,
-            .exchange = undefined,
         };
-        self.local = Local{ .w = &self };
-        self.exchange = Exchange{ .w = &self };
-        return self;
+    }
+
+    pub fn local(self: *World) *Local {
+        if (self._local == null) {
+            self._local = Local{ .w = self };
+        }
+        return &self._local.?;
+    }
+
+    pub fn exchange(self: *World) *Exchange {
+        if (self._exchange == null) {
+            self._exchange = Exchange{ .w = self };
+        }
+        return &self._exchange.?;
+    }
+
+    pub fn gotoTime(self: *World, time: ?i64) void {
+        const go_time = time orelse blk: {
+            const ex_time = self.ex_ep.fastest_finish_time;
+            const local_time = self.local_ep.fastest_finish_time;
+
+            break :blk if (ex_time) |ex|
+                if (local_time) |loc| @min(ex, loc) else ex
+            else
+                local_time;
+        };
+        if (go_time == null) return;
+        const ex_events = self.ex_ep.gotoTime(go_time);
+        const local_events = self.local_ep.gotoTime(go_time);
+        defer if (ex_events) |*events| events.deinit();
+        defer if (local_events) |*events| events.deinit();
+        try self.local().process_events(if (local_events) |events| events.items else null);
+        try self.exchange().process_events(if (ex_events) |events| events.items else null);
     }
 
     pub fn deinit(self: *World) void {
@@ -66,12 +103,15 @@ test "world base" {
         .{ .name = "ETH", .delay = .{ .send = 1, .receive = 1 } },
     };
     var world = World.init(.{ .assets = &asstes });
-    // defer world.deinit();
-    try world.local.newOrder(0, .{ .order_id = 1, .price = 10000, .qty = 1 });
-    // world.local.newOrder(1, .{ .order_id = 2, .price = 10000, .qty = 1 });
-    // world.local.newOrder(0, .{ .order_id = 3, .price = 10000, .qty = 1 });
-    // world.local.newOrder(1, .{ .order_id = 4, .price = 10000, .qty = 1 });
-    // std.debug.assert(world.ex_ep.len() == 4);
+    defer world.deinit();
+    try world.local().newOrder(0, .{ .order_id = 1, .price = 10000, .qty = 1 });
+    try world.local().newOrder(1, .{ .order_id = 2, .price = 10000, .qty = 1 });
+    try world.local().newOrder(0, .{ .order_id = 3, .price = 10000, .qty = 1 });
+    try world.local().newOrder(1, .{ .order_id = 4, .price = 10000, .qty = 1 });
+    std.debug.assert(world.ex_ep.len() == 4);
+    world.gotoTime(null);
+    std.debug.assert(world.ex_ep.len() == 0);
+    std.debug.assert(world.local_ep.len() == 0);
 }
 
 comptime {
--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,7 @@
 //! start with main.zig instead.
 const std = @import("std");
 const builtin = @import("builtin");
+pub const World = @import("backtest.zig").World;
 
 var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 
@@ -12,8 +13,6 @@ else switch (builtin.mode) {
     .Debug, .ReleaseSafe => debug_allocator.allocator(),
     .ReleaseFast, .ReleaseSmall => std.heap.smp_allocator,
 };
-
-// pub const test_allocator = if (@import("builtin").is_test) std.testing.allocator else null;
 
 comptime {
     std.testing.refAllDecls(@This());
